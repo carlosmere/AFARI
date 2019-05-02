03d9929697f319a3dd163711be7d1312412c7832
Carlos Mere <cmere@globokas.com>
2019-05-02 14:37:21 -0500

    login service

---
+++ TFS_IntranetAfari/VEH.Intranet/Models/BE/BaseBE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VEH.Intranet.Models.BE
+{
+    public class BaseBE
+    {
+        public bool error { get; set; } = false;
+        public string mensaje { get; set; } = String.Empty;
+    }
+}
---
+++ TFS_IntranetAfari/VEH.Intranet/Models/BE/ResponseLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VEH.Intranet.Models.BE
+{
+    public class ResponseLogin : BaseBE
+    {
+        public string nombre { get; set; }
+        public Int32 usuarioId { get; set; }
+        public string rol { get; set; }
+        public string correo { get; set; }
+        public Int32? departamentoId { get; set; }
+        public Int32? edificioId { get; set; }
+    }
+}
